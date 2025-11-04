31f08945e428be51ace0af7c1861862a496ae0dc
Maria Jenny Rosali <mjrosali@Marias-MacBook-Air.local>
2025-11-04 20:17:05 +0800

    Day 1: Added basics and math_sum activities – input/output, f-strings, and simple arithmetic.

---
+++ day01_basics/basics.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+favLang = input("Enter your favorite language: ")
+print(f"Cool! {favLang} is also my favorite.")
---
+++ day01_basics/math_sum.py
@@ -0,0 +1,4 @@
+stringFirstNum = input("Enter first number: ")
+stringSecondNum = input("Enter second number: ")
+nTotal = int(stringFirstNum) + int(stringSecondNum)
+print(f"The sum of {stringFirstNum} and {stringSecondNum} is {nTotal}.")
